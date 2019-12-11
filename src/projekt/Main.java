7b0b369b7696c64b3526963ee806fca36fc49264
Paweł Prusałowicz <pawel.prusalowicz@gmail.com>
2019-12-12 00:11:50 +0100

    lala

--- src/projekt/Main.java
+++ src/projekt/Main.java
@@ -3,7 +3,10 @@ package projekt;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("hello");
+        System.out.println("hello5");
+        System.out.println("hello5");
         System.out.println("hello5");
 
 
